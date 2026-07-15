8058cd0adb7d0a81a9b678839ddc0cf5061a5b5b
sultonialwi64 <sultonialwi64@gmail.com>
2026-07-15 20:07:07 +0700

    real payment

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // js/config.js
 export const APPS_SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbwITfc5pyZyq3mS6Ahm8nB3mN6aVVQ-CO1-d035d1GvJFw2lgQ_wBOIP4-X3NcpdcUC/exec";
+  "https://script.google.com/macros/s/AKfycbztpCNO_bGMcsnm01I9xrropCdPuXmdmQ7uHx4_8n4eW14S4lC4wrii1UDC5gM5saGC/exec";
